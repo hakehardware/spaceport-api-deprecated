53df740e07abc5da2894d5305471f356ce525657
hakehardware <alex@alexhake.com>
2024-08-19 23:14:29 +0000

    added docker files

--- api/api_routes.py
+++ api/api_routes.py
@@ -6,3 +6,7 @@ from .api_db import APIDB
 from logger import logger
 
 api_routes = Blueprint('api_routes', __name__)
+
+@api_routes.route('/hello', methods=['GET'])
+def hello():
+    return jsonify({"message": "Hi"}), 200
